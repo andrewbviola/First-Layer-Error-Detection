ffb78f5a65b93f5f16461fd68ef896917f9861d9
MinMaxolotl <98672212+MinMaxolotl@users.noreply.github.com>
2024-11-26 13:36:57 -0500

    Added Fourier Analysis for diagonal line frequency

--- CVProjEdge.py
+++ CVProjEdge.py
@@ -1,9 +1,8 @@
-import math
-import pandas as pd
 import numpy as np
 import cv2
 import skimage
 from tabulate import tabulate
+import scipy.fft
 import matplotlib.pyplot as plt
 
 photo = np.array(skimage.io.imread('./cv_photos/real.png'))
@@ -29,8 +28,8 @@ mask_crop = mask[:, 119:997]
 # mask_crop = mask[:, 130:983]
 # mask_crop = mask_crop[14:879, :]
 
-# skimage.io.imshow(mask_crop, cmap='gray')
-# skimage.io.show()
+skimage.io.imshow(mask_crop, cmap='gray')
+skimage.io.show()
 
 # thr1 = skimage.filters.threshold_otsu(photo)
 # thr2 = skimage.filters.threshold_otsu(mask_crop)
@@ -56,19 +55,19 @@ for row in range(92, 186):
     for col in range(369, 570):
         binaryimg1[row][col] = 0.0
 
-# skimage.io.imshow(binaryimg1, cmap='gray')
-# skimage.io.show()
-# skimage.io.imshow(binaryimg2, cmap='gray')
-# skimage.io.show()
+skimage.io.imshow(binaryimg1, cmap='gray')
+skimage.io.show()
+skimage.io.imshow(binaryimg2, cmap='gray')
+skimage.io.show()
 
 # get percent error
 # https://stackoverflow.com/questions/51288756/how-do-i-calculate-the-percentage-of-difference-between-two-images-using-python
 
-# for testing i black out one of the squares on the original photo
+# for testing, I black out one of the squares on the original photo
 
-for row in range(386, 483):
-    for col in range(386, 500):
-        binaryimg1[row][col] = 0.0
+# for row in range(386, 483):
+#     for col in range(386, 500):
+#         binaryimg1[row][col] = 0.0
 
 res = cv2.absdiff(binaryimg1, binaryimg2)
 #--- convert the result to integer type ---
@@ -78,11 +77,51 @@ ideal_nonzero = np.count_nonzero(binaryimg2)
 percentage = ((ideal_nonzero - (ideal_nonzero - np.count_nonzero(res))) * 100)/ ideal_nonzero
 print(f"percent error is: {percentage}%")
 
-skimage.io.imshow(res, cmap='gray')
+# skimage.io.imshow(res, cmap='gray')
+# skimage.io.show()
+
+# Now get only the ideal regions on the original photo to take fourier transform of
+masked_photo = np.zeros_like(photo_crop)
+for row in range(photo_crop.shape[0]):
+    for col in range(photo_crop.shape[1]):
+        if binaryimg2[row][col] == 255.0:
+            masked_photo[row][col] = photo_crop[row][col]
+
+skimage.io.imshow(masked_photo, cmap='gray')
 skimage.io.show()
 
+# take fourier transform of this photo 
 
+fourier_image = scipy.fft.fftshift(scipy.fft.fft2(masked_photo))
+#print(np.asarray(fourier_image).shape)
+skimage.io.imshow(20*np.log(np.abs(fourier_image)), cmap='gray')
+skimage.io.show()
 
+# Define area of diagonal frequency locations using an elliptical mask
+mask = np.zeros_like(fourier_image, dtype=np.uint8)
 
+# define center as center of quadrant II
+center = (int(mask.shape[1]/4), int(mask.shape[0]/4))
+# define axes as length of major axis and minor axis
+axes = (250, 50)
+# angle is rotation clockwise in degrees
+angle = 45
 
+cv2.ellipse(mask, center, axes, angle, 0, 360, (255, 0, 0), -1)
+
+result = cv2.bitwise_and(20*np.log(np.abs(fourier_image)), 20*np.log(np.abs(fourier_image)), mask=mask)
+
+skimage.io.imshow(mask, cmap='gray')
+skimage.io.show()
+
+skimage.io.imshow(result, cmap='gray')
+skimage.io.show()
+
+# Now find location of the maximum in the masked region
+max_index = np.unravel_index(result.argmax(), result.shape)
+print(f"Fourier maximum is at: ({max_index[1]}, {max_index[0]}) and is value {result[max_index[0]][max_index[1]]}")
+
+# Solve for wavelength which is 1/sqrt(u^2 + v^2)
+wavelength = 1/np.sqrt(pow(max_index[0], 2) + pow(max_index[1], 2))
+print(f"Wavelength is: {wavelength}")
 
