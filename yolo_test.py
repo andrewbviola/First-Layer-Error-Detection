3b89c0e52d250588c7e53331b3b73ad96580cbd9
MinMaxolotl <98672212+MinMaxolotl@users.noreply.github.com>
2024-12-03 14:54:45 -0500

    Tested on an unseen dataset of Spaghetti images

--- yolo_test.py
+++ yolo_test.py
@@ -1,26 +1,25 @@
-from tabnanny import check
-
 from ultralytics import YOLO
-import time
-import requests
+import torch
 
-# model = YOLO("best.pt")
-# # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
-# results = model.predict(source="./Results/spaghetti.jpg", show=False)
-# # results = model.predict(source="folder", show=True)  # Display preds. Accepts all YOLO predict arguments
-# for result in results:
-#     for box in result.boxes:  # Iterate through all detected boxes
-#         conf = box.conf[0]  # Confidence score
-#         print(f"Confidence: {conf}")
+def run():
+    print(torch.cuda.is_available())
+    torch.multiprocessing.freeze_support()
 
+    model = YOLO("./runs/detect/train14/weights/best.pt")
 
-def checkSpaghetti(img,pathModel):
-    model = YOLO(pathModel)
-    results = model.predict(img)
-    for result in results:
-        for box in result.boxes:  # Iterate through all detected boxes
-            if box.conf[0] > .75:
-                return True
-    return False
+    # for testing on individual images
+    # results = model.predict(source="./cv_photos/real.png", show=True)
+    # results = model.predict(source="folder", show=True)  # Display preds. Accepts all YOLO predict arguments
+    # results[0].show()
 
-checkSpaghetti("./Results/spaghetti.jpg","best.pt")
+    # Testing on the test dataset, was not used before in original training validation
+
+    metrics = model.val(data="C:/Users/maxwe/Documents/datasets/yolo_data/data.yaml",
+                        imgsz=640,                          # training image size
+                        device=0,                           # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+                        plots=True,                         # creates a plot of prediction vs ground truth  
+                        name="Test Dataset Evaluation",     # names the test
+                        split="test")                       # using test dataset
+
+if __name__ == '__main__':
+    run()
