887e5b9b8512db09da51724b75196aedac7a3c45
MinMaxolotl <98672212+MinMaxolotl@users.noreply.github.com>
2024-12-04 05:49:50 -0500

    Updated for clarity

--- yolo_test.py
+++ yolo_test.py
@@ -12,12 +12,7 @@ def run():
 
     model = YOLO("./runs/detect/NewerYOLO/weights/last.pt")
 
-    value = 0 # set to 1 for personal dataset, set to 0 for test dataset
-
-    # for testing on individual images
-    # results = model.predict(source="./cv_photos/real.png", show=True)
-    # results = model.predict(source="folder", show=True)  # Display preds. Accepts all YOLO predict arguments
-    # results[0].show()
+    value = 1 # set to 1 for personal dataset, set to 0 for test dataset
 
     # testing on the test dataset, images not used before in original training validation
     if value == 0:
@@ -27,8 +22,9 @@ def run():
                             name="Test Dataset Prediction",     # names the test
                             batch=1)                            # setting batchsize = 1
     
+    # testing on our pictures of the printbed
     elif value == 1:
-        # testing on our pictures of the printbed
+        
         results = model.predict("./Results",
                             imgsz=640,                          # testing image size
                             device=0,                           # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
@@ -39,42 +35,64 @@ def run():
     count = 0   
 
     for i in range(len(results)):
-        if len(results[i]) == value: 
-            count += 1
+        if value == 0:
+            if len(results[i]) == 0: # if value is zero, then we are using the online dataset
+                count += 1
 
-            fig, axes = plt.subplots(1, 2)
-            results[i].save("image.png")
-            img1 = np.array(Image.open("image.png"))
-            axes[0].imshow(img1)
-            axes[0].set_title("Mislabelled Photo")
-            axes[0].axis('off')
+                fig, axes = plt.subplots(1, 2)
+                results[i].save("image.png")
+                img1 = np.array(Image.open("image.png"))
+                axes[0].imshow(img1)
+                axes[0].set_title("Mislabelled Photo")
+                axes[0].axis('off')
 
-            img2 = np.array(Image.open(results[i].path))
+                img2 = np.array(Image.open(results[i].path))
 
-            if value == 0:
-                # print(i)
-                filepath = os.path.join("C:/Users/maxwe/Documents/datasets/yolo_data/test/labels", 
-                                    os.listdir("C:/Users/maxwe/Documents/datasets/yolo_data/test/labels")[i])
-                
-                with open(filepath, "r") as file:
-                    lines = file.readlines()
-                    for line in lines:
-                        x = int(float(line.split(" ")[1])*640)
-                        y = int(float(line.split(" ")[2])*640)
-                        # print(x)
-                        # print(y)
-                        width = int(float(line.split(" ")[3])*640)
-                        height = int(float(line.split(" ")[4])*640)
+                if value == 0:
+                    # print(i)
+                    filepath = os.path.join("C:/Users/maxwe/Documents/datasets/yolo_data/test/labels", 
+                                        os.listdir("C:/Users/maxwe/Documents/datasets/yolo_data/test/labels")[i])
 
-                        rect = patches.Rectangle(((x-0.5*width), (y+0.5*height)), width, -height, linewidth=2, edgecolor='b', facecolor='none')
-                        axes[1].add_patch(rect)
-                
+                    # https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/#
+                    with open(filepath, "r") as file:
+                        lines = file.readlines()
+                        for line in lines:
+                            # https://stackoverflow.com/questions/66005018/getting-the-second-word-in-a-python-3-string
+                            x = int(float(line.split(" ")[1])*640)
+                            y = int(float(line.split(" ")[2])*640)
+                            # print(x)
+                            # print(y)
+                            width = int(float(line.split(" ")[3])*640)
+                            height = int(float(line.split(" ")[4])*640)
 
-            axes[1].imshow(img2)
-            axes[1].set_title("Original Photo")
-            axes[1].axis('off')
+                            rect = patches.Rectangle(((x-0.5*width), (y+0.5*height)), width, -height, linewidth=2, edgecolor='b', facecolor='none')
+                            axes[1].add_patch(rect)
 
-            plt.show()
+
+                axes[1].imshow(img2)
+                axes[1].set_title("Original Photo")
+                axes[1].axis('off')
+
+                plt.show()
+
+        elif value == 1:
+            if len(results[i]) > 0:
+                count += 1
+                fig, axes = plt.subplots(1, 2)
+                results[i].save("image.png")
+                img1 = np.array(Image.open("image.png"))
+                axes[0].imshow(img1)
+                axes[0].set_title("Mislabelled Photo")
+                axes[0].axis('off')
+
+                img2 = np.array(Image.open(results[i].path))
+
+                axes[1].imshow(img2)
+                axes[1].set_title("Original Photo")
+                axes[1].axis('off')
+
+                plt.show()
+
     
     percent_error = count / len(results)
     accuracy = (1 - percent_error) * 100
